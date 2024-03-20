a3265c096a516c458f2d7b85461245194f44f1a7
Calvin Garcia <calvinmgarcia@hotmail.com>
2024-03-20 14:49:21 -0400

    Added test level #2

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -39,9 +39,29 @@ public class Goal : MonoBehaviour
     }
 
     // Method to replay the current level
-    public void ReplayLevel()
+    public void Level1()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level5");
     }
 
     // Method to go back to the level selection screen
