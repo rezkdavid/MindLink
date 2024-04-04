c8d976f94d363e77cfd1d69de0808ed626bad321
Calvin Garcia <calvinmgarcia@hotmail.com>
2024-04-03 22:47:22 -0400

    Added animated player sprite and re-worked goalScript a bit and also added level 3 prototype

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    // User-defined speed variable
+    public float userSpeed = 15f;
 
-    private SpriteRenderer spriteRenderer; 
+    private float moveSpeed = 0f;
+
+    private SpriteRenderer spriteRenderer;
+    public Animator animator;
 
     void Start()
     {
@@ -14,13 +18,27 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         float direction = 0f;
+
         if (Input.GetKey(KeyCode.A))
+        {
             direction = -1f; // Move left when 'A' is pressed
+            moveSpeed = userSpeed; // Set moveSpeed to userSpeed
+            spriteRenderer.flipX = true; // Flip sprite to face left
+        }
         else if (Input.GetKey(KeyCode.D))
+        {
             direction = 1f; // Move right when 'D' is pressed
+            moveSpeed = userSpeed; // Set moveSpeed to userSpeed
+            spriteRenderer.flipX = false; // Do not flip sprite to face right
+        }
+        else
+        {
+            moveSpeed = 0f; // Set moveSpeed to 0 if no keys are pressed
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(moveSpeed));
 
         Vector3 movement = new Vector3(direction * moveSpeed * Time.deltaTime, 0f, 0f);
         transform.Translate(movement);
     }
-
 }
