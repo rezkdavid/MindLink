eb1202aa318e6ff826fe980c5dcd2ca27c96a1d7
Hanbin Qin <610757729@qq.com>
2024-04-24 00:31:36 -0400

    add websocket client, modified playerScript

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,34 +2,47 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // User-defined speed variable
-    public float userSpeed = 5f;
-
+    public float userSpeed = 5.0f;
     private float moveSpeed = 0f;
-
     private SpriteRenderer spriteRenderer;
-    public Animator animator;
+    private Animator animator;
+
+    // external controlled direction
+    private float externalDirection = 0f;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // keyboard control
         float direction = 0f;
-
         if (Input.GetKey(KeyCode.A))
         {
             direction = -1f; // Move left when 'A' is pressed
-            moveSpeed = userSpeed; // Set moveSpeed to userSpeed
-            spriteRenderer.flipX = true; 
         }
         else if (Input.GetKey(KeyCode.D))
         {
             direction = 1f; // Move right when 'D' is pressed
+        }
+        else
+        {
+            direction = externalDirection; // use external control
+        }
+
+        // set speed and direction
+        UpdateMovement(direction);
+    }
+
+    private void UpdateMovement(float direction)
+    {
+        if (direction != 0)
+        {
             moveSpeed = userSpeed; // Set moveSpeed to userSpeed
-            spriteRenderer.flipX = false; 
+            spriteRenderer.flipX = direction < 0; // Flip sprite based on direction
         }
         else
         {
@@ -40,5 +53,11 @@ public class PlayerScript : MonoBehaviour
 
         Vector3 movement = new Vector3(direction * moveSpeed * Time.deltaTime, 0f, 0f);
         transform.Translate(movement);
+    }
+
+    // External direction setter, used by WebSocket
+    public void SetDirection(float dir)
+    {
+        externalDirection = dir;
     }
 }
